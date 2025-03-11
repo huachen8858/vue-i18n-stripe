1329b929969c8f861a229eb6494c59c62b13ca37
huachen8858 <huachen8858@gmail.com>
2025-03-11 22:04:35 +0800

    i18n init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createI18n } from "vue-i18n";
+import EN from "./locale/en.json";
+import ZHTW from "./locale/zhTw.json";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const i18n = createI18n(
+  {
+    locale: document.cookie.split('locale=')[1],
+    fallbackLocale: "EN",
+    messages: {
+      EN: EN,
+      ZHTW: ZHTW,
+    },
+  }
+);
+
+app.use(i18n);
+app.mount("#app");
