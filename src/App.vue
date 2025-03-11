6d00bd54c2210771f3dde996a333b29323001d64
huachen8858 <huachen8858@gmail.com>
2025-03-11 22:13:51 +0800

    add select option & switch language function

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  H:{{ $t("hello") }}
-  
+  {{ $t("hello") }}
+  <select
+    name="locale"
+    id="locale"
+    @change="SwitchLanguage"
+    v-model="currentLocale"
+  >
+    <option value="EN">{{ $t("EN") }}</option>
+    <option value="ZHTW">{{ $t("ZHTW") }}</option>
+  </select>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from "vue";
+import { useI18n } from "vue-i18n";
 
-onMounted(()=>{
-  document.cookie = 'locale=EN'
+const { locale } = useI18n();
+const currentLocale = ref("EN");
+
+const SwitchLanguage = (event) => {
+  const selectedLang = event.target.value;
+  document.cookie = `locale=${selectedLang}; path=/`;
+  locale.value = selectedLang;
+};
+
+onMounted(() => {
+  const savedLocale =
+    document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("locale="))
+      ?.split("=")[1] || "EN";
+  currentLocale.value = savedLocale;
+  locale.value = savedLocale;
 });
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
